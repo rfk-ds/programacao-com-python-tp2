0319e29bbc5a5eb9a712fd17a964b4f2d55d925b
Rafiki <rafikidias.cy@gmail.com>
2024-08-25 03:27:10 -0300

    feat(imprime_mensagem)

---
+++ nivel_minhoquinha/nm4.py
@@ -0,0 +1,7 @@
+# Defina uma função chamada imprime_mensagem que receba uma mensagem como argumento e a imprima na tela.
+
+def imprime_mensagem(entrada):
+    print(entrada)
+
+entrada = input("Digite sua mensagem: ")
+imprime_mensagem(entrada)
